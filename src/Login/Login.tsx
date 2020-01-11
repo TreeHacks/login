9a825498249928368e01dd22a8a13f67dcf3f941
Ashwin Ramaswami <aramaswamis@gmail.com>
2020-01-11 00:53:41 +0000

    remove hyperlink from image

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -122,11 +122,11 @@ export class Login extends React.Component<ILoginProps, { formData: any, sponsor
     if (!this.props.loggedIn) {
       return (<div className="login-container"><div className="treehacks-login">
         <div className="text-center">
-          <a href="https://treehacks.com/">
+          <a href="">
             <img src={logo} width="85px" height="65px" style={{ "marginTop": 49 }} />
           </a>
         </div>
-        <a href="https://treehacks.com/" className="wordmark">
+        <a href="" className="wordmark">
           <h2 className="h3-style">tree<strong>hacks</strong></h2>
         </a>
         {this.state.sponsor && <h3 className="h3-style">sponsors</h3>}
