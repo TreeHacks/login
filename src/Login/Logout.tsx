0e15b8dce076db544dd760646a9158b57df03e3e
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-10-27 15:26:44 -0700

    Update Logout.tsx put back hacky fix

--- src/Login/Logout.tsx
+++ src/Login/Logout.tsx
@@ -14,9 +14,9 @@ const Logout = ({ logout }) => {
     useEffect(() => {
         logout();
         // todo: hacky fix. we should redirect only when we we know that logout() has finished.
-        //setTimeout(() => {
-        //    window.location.href = getRedirectUrl();
-        //}, 500);
+        setTimeout(() => {
+            window.location.href = getRedirectUrl();
+        }, 500);
     });
     return <div>You are now logged out.<br />
         <a href={getRedirectUrl()}>Continue to the next page</a> <br />
